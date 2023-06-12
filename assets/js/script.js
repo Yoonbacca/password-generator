d5aedd304ec315ab636342095a3849ff77d80330
Yoonbacca <yoonjaekim3@gmail.com>
2023-06-12 17:11:30 -0500

    Added prompt function to script sheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add prompts after button click
+// Function based on w3 schools example at https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt
+function passPrompt() {
+  let text;
+  let person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
